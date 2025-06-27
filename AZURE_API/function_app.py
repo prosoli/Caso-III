382f2d7cc4b21b54759aa13e56682f42b935b36a
Daniel Sere <87248193+DanielSere@users.noreply.github.com>
2025-06-26 23:46:06 -0600

    Update function_app.py

--- AZURE_API/function_app.py
+++ AZURE_API/function_app.py
@@ -4,8 +4,10 @@ from Endpoints_SP.get_usuarios_sp import get_usuarios_sp
 from Endpoints_SP.invertir_sp import invertir
 from Endpoints_SP.revisarPropuesta_sp import revisarPropuesta_sp
 
+from Endpoints_SP.crearActualizarPropuesta import crearActualizarPropuesta
 from Endpoints_ORM.get_usuarios_orm import get_usuarios_orm
 from Endpoints_ORM.configurarVotacion_orm import configurarVotacionORM
+
 
 
 
@@ -15,6 +17,10 @@ app = func.FunctionApp()
 #Aca se definen las rutas que seran parte del api
 
 #EndPoint por SP-------------------------------------------------------------------------------------------------
+@app.function_name(name="crearActualizarPropuesta")
+@app.route(route="crearActualizarPropuesta", methods=["POST"], auth_level=func.AuthLevel.ANONYMOUS)
+def run_crearActualizarPropuesta(req: func.HttpRequest) -> func.HttpResponse:
+    return crearActualizarPropuesta(req)
 app.route(route="get_usuarios_sp", auth_level=func.AuthLevel.ANONYMOUS)(get_usuarios_sp) #EJEMPLO poner el nombre del archivo importado
 app.route(route="invertir", auth_level=func.AuthLevel.ANONYMOUS)(invertir)
 app.route(route="revisarPropuesta_sp", auth_level=func.AuthLevel.ANONYMOUS)(revisarPropuesta_sp)
