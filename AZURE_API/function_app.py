cd59694d98e3ac7909eaf03dc70bfe929137c658
Daniel Sere <87248193+DanielSere@users.noreply.github.com>
2025-06-27 05:28:42 -0600

    Update function_app.py

--- AZURE_API/function_app.py
+++ AZURE_API/function_app.py
@@ -2,11 +2,12 @@ import azure.functions as func
 
 
 from Endpoints_SP.get_usuarios_sp import get_usuarios_sp
+
 from Endpoints_SP.crearActualizarPropuesta import crearActualizarPropuesta
 from Endpoints_SP.invertir_sp import invertir
 from Endpoints_SP.revisarPropuesta_sp import revisarPropuesta_sp
 from Endpoints_ORM.comentar_orm import comentar
-
+from Endpoints_SP.repartir_dividendos import repartir_dividendos
 
 
 from Endpoints_ORM.get_usuarios_orm import get_usuarios_orm
@@ -31,6 +32,7 @@ def run_crearActualizarPropuesta(req: func.HttpRequest) -> func.HttpResponse:
 app.route(route="get_usuarios_sp", auth_level=func.AuthLevel.ANONYMOUS)(get_usuarios_sp) #EJEMPLO poner el nombre del archivo importado
 app.route(route="invertir", auth_level=func.AuthLevel.ANONYMOUS)(invertir)
 app.route(route="revisarPropuesta_sp", auth_level=func.AuthLevel.ANONYMOUS)(revisarPropuesta_sp)
+app.route(route='repartir_dividendos', auth_level=func.AuthLevel.ANONYMOUS)(repartir_dividendos)
 
 #EndPoint con ORM------------------------------------------------------------------------------------------------
 app.route(route="get_usuarios_orm", auth_level=func.AuthLevel.ANONYMOUS)(get_usuarios_orm)
